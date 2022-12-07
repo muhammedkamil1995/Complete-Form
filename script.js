eb5993692ecdc381def0c273e4432f84864eddad
Otunba Adetunji <kamzycoded@gmail.com>
2022-12-07 03:48:40 -0800

    Added my project

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+// Select all input elements for varification
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("phoneNumber");
+const gender = document.registration;
+const country = document.getElementById("language");
+const username = document.getElementById("name");
+const password = document.getElementById("password");
+
 const slidePage = document.querySelector(".slide-page");
 const nextBtnFirst = document.querySelector(".firstNext");
 const prevBtnSec = document.querySelector(".prev-1");
@@ -11,6 +21,16 @@ const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
 let current = 1;
 
+// function for form varification
+function formValidation() 
+  
+  // checking name length
+  if (name.value.length < 2 || name.value.length > 20) {
+    alert("Name length should be more than 2 and less than 21");
+    name.focus();
+    return false;
+  }
+
 nextBtnFirst.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-25%";
@@ -19,6 +39,7 @@ nextBtnFirst.addEventListener("click", function(event){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
+
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
@@ -69,4 +90,4 @@ prevBtnFourth.addEventListener("click", function(event){
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
   current -= 1;
-});
+}); 
